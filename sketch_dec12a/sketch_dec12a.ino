b458b28eb68a7c50c995bf3210820124afced39a
yamaan93 <yamaan93@gmail.com>
2017-12-18 10:12:05 -0500

    ugh
    took out button code from 12 but still giving zeros so trying new code

--- sketch_dec12a/sketch_dec12a.ino
+++ sketch_dec12a/sketch_dec12a.ino
@@ -2,17 +2,7 @@
 #define accel_module (0x53)
 byte values[6];
 char output[512];
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
-int xCal;
-int yCal;
-int zCal;
-int calX;
-int calY;
-int calZ;
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
 void setup() {
   Wire.begin();
   Serial.begin(9600);
@@ -28,8 +18,7 @@ void setup() {
   Wire.write(0x2D);
   Wire.write(8);
   Wire.endTransmission();
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  
 }
 
 
@@ -55,17 +44,10 @@ void loop() {
   y = (((int)values[3]) << 8) | values[2];
   z = (((int)values[5]) << 8) | values[4];
 
-  sprintf(output, "%d %d %d %d", x , y, z, buttonState);
+  sprintf(output, "%d %d %d", x , y, z);
   Serial.print(output);
   Serial.write(10);
-  buttonState = digitalRead(buttonPin);
-  if ( buttonState == 1) {
-    digitalWrite(ledPin, 1);
-//  cal();
-  }
-  else {
-    digitalWrite(ledPin, 0);
-  }
+
   delay(1000);
 }
 /*void cal() { 
